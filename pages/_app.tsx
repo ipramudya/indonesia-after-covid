c4ef205ca8788f3992cfeb1c01bfacc55eeaaa79
ipramudya <ipramudya.dev@gmail.com>
2022-03-21 16:30:50 +0700

    Add progress bar when route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,41 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
-
+import NProgress from "nprogress";
 import { CasesProvider } from "context/cases/cases.context";
 import { VaccineProvider } from "context/vaccine/vaccine.context";
 import theme from "lib/mantine/theme";
+
 import "mapbox-gl/dist/mapbox-gl.css";
 import "styles/recharts.css";
 import "styles/mantine.css";
 import "styles/mapbox.css";
+import "styles/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleStart = (url: any) => {
+            console.log(`Loading: ${url}`);
+            NProgress.start();
+        };
+        const handleStop = () => {
+            NProgress.done();
+        };
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleStop);
+        router.events.on("routeChangeError", handleStop);
+
+        return () => {
+            router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleStop);
+            router.events.off("routeChangeError", handleStop);
+        };
+    }, [router]);
+
     return (
         <MantineProvider withNormalizeCSS withGlobalStyles theme={theme}>
             <CasesProvider>
