aec6cc0444e7e44f767109b9c4dd2538dfeb58cd
ipramudya <ipramudya.dev@gmail.com>
2022-10-10 23:20:33 +0700

    chore: next config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,4 @@ const nextConfig = {
 
 module.exports = {
     nextConfig,
-    env: {
-        MAPBOX_ACCESS_TOKEN:
-            "sk.eyJ1IjoiaXByYW11ZHlhMCIsImEiOiJjbDBrMnR1bzYwaWdlM2t1b3M5YXpnZ3lpIn0.GTgUgbvdCAiTvJ0X-22oig",
-    },
 };
